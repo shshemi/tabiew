b27030379f02e11b2458a8bb3d7727d9cc04078d
Shayan Hashemi <shshemi@gmail.com>
2024-05-01 17:38:38 +0300

    add event handling in different state

--- src/handler.rs
+++ src/handler.rs
@@ -1,27 +1,22 @@
-use crate::app::{App, AppResult};
+use crate::app::{App, AppResult, AppStatus};
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 
 /// Handles the key events and updates the state of [`App`].
 pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
-    match key_event.code {
+    match (&app.status, key_event.code) {
         // Exit application on `ESC` or `q`
-        KeyCode::Esc | KeyCode::Char('q') => {
-            app.quit();
-        }
-        // Exit application on `Ctrl-C`
-        KeyCode::Char('c') | KeyCode::Char('C') => {
-            if key_event.modifiers == KeyModifiers::CONTROL {
-                app.quit();
-            }
+        (_, KeyCode::Esc) => {
+            app.state_normal();
         }
 
-        KeyCode::Up => app.select_up(1),
-
-        KeyCode::Down => app.select_down(1),
-
-        KeyCode::PageUp => app.select_up(app.visible_rows.into()),
-
-        KeyCode::PageDown => app.select_down(app.visible_rows.into()),
+        (AppStatus::Normal, KeyCode::Char('q')) => app.quit(),
+        (AppStatus::Normal, KeyCode::Up) => app.select_up(1),
+        (AppStatus::Normal, KeyCode::Down) => app.select_down(1),
+        (AppStatus::Normal, KeyCode::PageUp) => app.select_up(app.visible_rows.into()),
+        (AppStatus::Normal, KeyCode::PageDown) => app.select_down(app.visible_rows.into()),
+        (AppStatus::Normal, KeyCode::Char(':')) => {
+            app.state_error("Commands are not supported yet!".to_owned(), 8)
+        }
 
         _ => {}
     }
