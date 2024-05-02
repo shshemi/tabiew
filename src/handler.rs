1dfd1859b8e47c906f349ebf7401d5f303b31fed
Shayan Hashemi <shshemi@gmail.com>
2024-05-02 22:18:55 +0300

    make the table widget be creating in rendering time

--- src/handler.rs
+++ src/handler.rs
@@ -28,8 +28,8 @@ pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
         (AppStatus::Normal, KeyCode::Char('q')) => app.quit(),
         (AppStatus::Normal, KeyCode::Up) => app.select_up(1),
         (AppStatus::Normal, KeyCode::Down) => app.select_down(1),
-        (AppStatus::Normal, KeyCode::PageUp) => app.select_up(app.visible_rows.into()),
-        (AppStatus::Normal, KeyCode::PageDown) => app.select_down(app.visible_rows.into()),
+        (AppStatus::Normal, KeyCode::PageUp) => app.select_up(app.rendered_rows.into()),
+        (AppStatus::Normal, KeyCode::PageDown) => app.select_down(app.rendered_rows.into()),
         (AppStatus::Normal, KeyCode::Home) => app.select_first(),
         (AppStatus::Normal, KeyCode::End) => app.select_last(),
         (AppStatus::Normal, KeyCode::Char(':')) => {
