858ac01549e7c01f0291ff3e1c8cc745f49a4c9a
Shayan Hashemi <shshemi@gmail.com>
2024-08-30 02:20:37 +0300

    Revert "clean up"
    This reverts commit aec5a6d10c0de1a5409a9e6c211c87d445b72ab7.

--- src/tui/utils.rs
+++ src/tui/utils.rs
@@ -1,4 +1,97 @@
-use polars::{prelude::AnyValue, series::Series};
+use polars::{frame::DataFrame, prelude::AnyValue, series::Series};
+
+use crate::utils::RoundRobinExt;
+
+#[derive(Debug)]
+pub struct TableValues {
+    pool: ValuePool,
+    height: usize,
+    width: usize,
+}
+
+impl TableValues {
+    pub fn from_dataframe(data_frame: &DataFrame) -> Self {
+        let height = data_frame.height();
+        let width = data_frame.width();
+        let pool = data_frame.iter().map(Series::iter).round_robin().collect();
+        Self {
+            pool,
+            width,
+            height,
+        }
+    }
+
+    pub fn get(&self, row: usize, col: usize) -> Option<&str> {
+        self.pool.get(row * self.width + col)
+    }
+
+    pub fn get_row(&self, row: usize) -> Vec<&str> {
+        (0..self.width)
+            .map(|col| self.get(row, col).unwrap_or(""))
+            .collect()
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn height(&self) -> usize {
+        self.height
+    }
+
+    pub fn replace_dataframe(&mut self, data_frame: &DataFrame) {
+        self.height = data_frame.height();
+        self.width = data_frame.width();
+        self.pool.clear();
+        data_frame
+            .iter()
+            .map(Series::iter)
+            .round_robin()
+            .for_each(|value| self.pool.push(value));
+    }
+}
+
+#[derive(Debug)]
+struct ValuePool {
+    pool: String,
+    indices: Vec<usize>,
+}
+
+impl ValuePool {
+    pub fn new(pool_capacity: usize, index_capacity: usize) -> Self {
+        let pool = String::with_capacity(pool_capacity);
+        let mut indices = Vec::with_capacity(index_capacity);
+        indices.push(0);
+        Self { pool, indices }
+    }
+
+    pub fn push(&mut self, value: AnyValue) {
+        self.pool.push_str(&any_value_into_string(value));
+        self.indices.push(self.pool.len());
+    }
+
+    pub fn get(&self, index: usize) -> Option<&str> {
+        let start = *self.indices.get(index)?;
+        let end = *self.indices.get(index + 1)?;
+        self.pool.get(start..end)
+    }
+
+    pub fn clear(&mut self) {
+        self.pool.clear();
+        self.indices.clear();
+        self.indices.push(0);
+    }
+}
+
+impl<'a> FromIterator<AnyValue<'a>> for ValuePool {
+    fn from_iter<T: IntoIterator<Item = AnyValue<'a>>>(iter: T) -> Self {
+        let mut pool = ValuePool::new(16, 16);
+        for value in iter {
+            pool.push(value);
+        }
+        pool
+    }
+}
 
 #[derive(Debug, Default, Clone)]
 pub struct Scroll(usize);
@@ -84,8 +177,21 @@ pub fn any_value_into_string(value: polars::datatypes::AnyValue) -> String {
 
 #[cfg(test)]
 mod tests {
+    use polars::df;
+
     use super::*;
 
+    #[test]
+    fn test_value_pool() {
+        let df = df! {
+            "ints" => [1,2,3],
+            "floats" => [1.0,2.0,3.0],
+        }
+        .unwrap();
+
+        let vp = TableValues::from_dataframe(&df);
+        println!("{:?}", vp.get(0, 0))
+    }
 
     #[test]
     fn test_line_count_single_line() {
