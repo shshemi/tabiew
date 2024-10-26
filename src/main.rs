1b988d96d8e76179f17833dcd23b8824f3bf0c51
Shayan Hashemi <shshemi@gmail.com>
2024-10-27 00:13:42 +0300

    add stdin as a type of input

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use clap::Parser;
+use itertools::Itertools;
 use polars::frame::DataFrame;
 use ratatui::backend::CrosstermBackend;
-use std::io::{self};
+use std::fs::File;
+use std::io::{self, Cursor, Read};
+use std::path::PathBuf;
+use std::str::FromStr;
 use tabiew::app::App;
 use tabiew::args::{AppTheme, Args};
 use tabiew::handler::command::Commands;
@@ -22,7 +26,7 @@ fn main() -> AppResult<()> {
 
     // Loading files to data frames
     let reader = args.build_reader();
-    let tabs = args
+    let mut tabs = args
         .files
         .iter()
         .map(|path| {
@@ -33,12 +37,20 @@ fn main() -> AppResult<()> {
                 .into_owned();
 
             let df = reader
-                .read_to_data_frame(path.clone())
+                .read_to_data_frame(File::open(path).unwrap_or_else(|err| panic!("{}", err)))
                 .unwrap_or_else(|err| panic!("{}", err));
             let name = sql_backend.register(&name, df.clone(), path.clone());
             (df, name)
         })
-        .collect();
+        .collect_vec();
+    if tabs.is_empty() {
+        let reader = args.build_reader();
+        let mut buf = Vec::new();
+        io::stdin().read_to_end(&mut buf)?;
+        let df = reader.read_to_data_frame(Cursor::new(buf))?;
+        let name = sql_backend.register("stdin", df.clone(), PathBuf::from_str("stdin")?);
+        tabs.push((df, name));
+    }
 
     match args.theme {
         AppTheme::Monokai => start_tui::<themes::Monokai>(tabs, sql_backend),
