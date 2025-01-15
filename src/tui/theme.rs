90ca25e9d746f5e54abd757a7f31aa207b0926a8
Shayan Hashemi <shayan.hashemi@gmail.com>
2025-01-15 17:22:19 +0200

    add catppuccin theme

--- src/tui/theme.rs
+++ src/tui/theme.rs
@@ -128,6 +128,7 @@ pub struct Monokai;
 pub struct Argonaut;
 pub struct Terminal;
 pub struct Nord;
+pub struct Catppuccin;
 
 impl SixColorsTwoRowsStyler for Monokai {
     const BACKGROUND: Color = Color::from_u32(0x001c191d);
@@ -231,6 +232,46 @@ impl SixColorsTwoRowsStyler for Nord {
     const STATUS_BAR_SEARCH: Color = Color::from_u32(0x005E81AC);
 
     const STATUS_BAR_INFO: Color = Color::from_u32(0x002E3440);
+}
+
+impl SixColorsTwoRowsStyler for Catppuccin {
+    const BACKGROUND: Color = Color::from_u32(0x0011111b);
+
+    const LIGHT_BACKGROUND: Color = Color::from_u32(0x001e1e2e);
+
+    const FOREGROUND: Color = Color::from_u32(0x00cdd6f4);
+
+    const COLORS: [Color; 6] = [
+        Color::from_u32(0x00cba6f7),
+        Color::from_u32(0x00f38ba8),
+        Color::from_u32(0x00fab387),
+        Color::from_u32(0x00a6e3a1),
+        Color::from_u32(0x0074c7ec),
+        Color::from_u32(0x0089b4fa),
+    ];
+
+    const DARK_COLORS: [Color; 6] = [
+        Color::from_u32(0x00cba6f7),
+        Color::from_u32(0x00f38ba8),
+        Color::from_u32(0x00fab387),
+        Color::from_u32(0x00a6e3a1),
+        Color::from_u32(0x0074c7ec),
+        Color::from_u32(0x0089b4fa),
+    ];
+
+    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00181825), Color::from_u32(0x001e1e2e)];
+
+    const HIGHTLIGHT_BACKGROUND: Color = Color::from_u32(0x00f9e2af);
+
+    const HIGHTLIGHT_FOREGROUND: Color = Color::from_u32(0x0011111b);
+
+    const STATUS_BAR_ERROR: Color = Color::from_u32(0x00d36b98);
+
+    const STATUS_BAR_PROMPT: Color = Color::from_u32(0x0011111b);
+
+    const STATUS_BAR_SEARCH: Color = Color::from_u32(0x004497bc);
+
+    const STATUS_BAR_INFO: Color = Color::from_u32(0x0011111b);
 }
 
 impl Styler for Terminal {
