ee1eba7c229babe8f2d0cd5d49cd17b8efa94bb0
Shayan Hashemi <shayan.hashemi@gmail.com>
2025-05-08 00:12:15 +0300

    add custom date inference

--- src/misc/polars_ext.rs
+++ src/misc/polars_ext.rs
@@ -1,9 +1,16 @@
-use std::ops::Div;
+use std::{
+    ops::Div,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+};
 
+use chrono::NaiveDateTime;
 use itertools::Itertools;
 use polars::{
     frame::DataFrame,
-    prelude::{AnyValue, DataType, TimeUnit},
+    prelude::{AnyValue, DataType, NamedFrom, TimeUnit},
     series::{ChunkCompareEq, Series},
 };
 use unicode_width::UnicodeWidthStr;
@@ -47,17 +54,123 @@ impl SafeInferSchema for DataFrame {
 }
 
 fn type_infered_series(series: &Series) -> Option<Series> {
-    [
-        DataType::Int64,
-        DataType::Float64,
-        DataType::Boolean,
-        DataType::Date,
-        DataType::Time,
-        DataType::Datetime(TimeUnit::Milliseconds, None),
-    ]
-    .iter()
-    .filter_map(|dtype| series.cast(dtype).ok())
-    .find(|dtype_series| series.is_null().equal(&dtype_series.is_null()).all())
+    [cast_int64, cast_float64, cast_boolean, cast_datetime]
+        .iter()
+        .find_map(|func_map| func_map(series))
+}
+
+fn cast_int64(series: &Series) -> Option<Series> {
+    let new_series = series.cast(&DataType::Int64).ok()?;
+    series
+        .is_null()
+        .equal(&new_series.is_null())
+        .all()
+        .then_some(new_series)
+}
+
+fn cast_float64(series: &Series) -> Option<Series> {
+    let new_series = series.cast(&DataType::Float64).ok()?;
+    series
+        .is_null()
+        .equal(&new_series.is_null())
+        .all()
+        .then_some(new_series)
+}
+
+fn cast_boolean(series: &Series) -> Option<Series> {
+    let new_series = series.cast(&DataType::Boolean).ok()?;
+    series
+        .is_null()
+        .equal(&new_series.is_null())
+        .all()
+        .then_some(new_series)
+}
+
+fn cast_datetime(series: &Series) -> Option<Series> {
+    cast_datetime_polars(series).or_else(|| {
+        [
+            "%Y-%m-%d %H:%M:%S",
+            "%Y-%m-%dT%H:%M:%S",
+            "%Y-%m-%dT%H:%M:%S%.f",
+            "%Y%m%dT%H%M%S",
+            "%Y-%m-%d",
+            "%Y %m %d",
+            "%Y.%m.%d",
+            "%Y%m%d",
+            "%Y-%j",
+        ]
+        .into_iter()
+        .find_map(|fmt| cast_datetime_custom(series, fmt))
+    })
+}
+
+fn cast_datetime_polars(series: &Series) -> Option<Series> {
+    let new_series = series
+        .cast(&DataType::Datetime(TimeUnit::Milliseconds, None))
+        .ok()?;
+    series
+        .is_null()
+        .equal(&new_series.is_null())
+        .all()
+        .then_some(new_series)
+}
+
+fn cast_datetime_custom(series: &Series, fmt: &'static str) -> Option<Series> {
+    cast_custom(series, |val| match val {
+        AnyValue::String(s) => parse_datetime(s, fmt),
+        AnyValue::StringOwned(s) => parse_datetime(s.as_str(), fmt),
+        AnyValue::Datetime(ts, unit, zone) => Some(AnyValue::DatetimeOwned(
+            ts,
+            unit,
+            zone.map(|sm| sm.to_owned().into()),
+        )),
+        AnyValue::DatetimeOwned(ts, unit, zone) => Some(AnyValue::DatetimeOwned(ts, unit, zone)),
+        AnyValue::Null => Some(AnyValue::Null),
+        _ => None,
+    })
+}
+
+fn parse_datetime(slice: &str, fmt: &str) -> Option<AnyValue<'static>> {
+    NaiveDateTime::parse_from_str(slice, fmt)
+        .map(|date| {
+            AnyValue::DatetimeOwned(
+                date.and_utc().timestamp_millis(),
+                TimeUnit::Milliseconds,
+                None,
+            )
+        })
+        .ok()
+}
+
+fn cast_custom(
+    series: &Series,
+    cast: impl Fn(AnyValue) -> Option<AnyValue<'static>> + Sync + Send + 'static,
+) -> Option<Series> {
+    let break_out = Arc::new(AtomicBool::new(false));
+    let mut new = vec![AnyValue::Null; series.len()];
+    std::thread::scope(|scope| {
+        let piece_len = series.len() / num_cpus::get();
+        for (idx, new_chunk) in new.chunks_mut(piece_len).enumerate() {
+            let offset = (idx * piece_len) as i64;
+            let break_out = break_out.clone();
+            let cast = &cast;
+            scope.spawn(move || {
+                let series = series.slice(offset, piece_len);
+                for (new_val, val) in new_chunk.iter_mut().zip(series.iter()) {
+                    if let Some(parsed) = cast(val) {
+                        *new_val = parsed;
+                    } else {
+                        break_out.store(true, Ordering::Relaxed);
+                        break;
+                    }
+                    if break_out.load(Ordering::Relaxed) {
+                        break;
+                    }
+                }
+            });
+        }
+    });
+    (!break_out.load(Ordering::Relaxed)).then_some(Series::new(series.name().to_owned(), new))
 }
 
 impl IntoString for AnyValue<'_> {
