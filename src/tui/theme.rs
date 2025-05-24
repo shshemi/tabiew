eda7a05848bbf3f0e1ea8a5f9c0954ed60ead082
Shayan Hashemi <shayan.hashemi@gmail.com>
2025-05-24 03:10:17 +0300

    Add Chakra theme support

--- src/tui/theme.rs
+++ src/tui/theme.rs
@@ -108,6 +108,9 @@ pub struct Catppuccin;
 #[derive(Debug, Default)]
 pub struct TokyoNight;
 
+#[derive(Debug, Default)]
+pub struct Chakra;
+
 impl SixColorsTwoRowsStyler for Monokai {
     const BACKGROUND: Color = Color::from_u32(0x00141115);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x003e3b3f);
@@ -252,6 +255,33 @@ impl SixColorsTwoRowsStyler for TokyoNight {
     const HIGHTLIGHT_BACKGROUND: Color = Color::from_u32(0x00ffc777);
     const HIGHTLIGHT_FOREGROUND: Color = Color::from_u32(0x001f2335);
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x00c53b53);
+}
+
+impl SixColorsTwoRowsStyler for Chakra {
+    const BACKGROUND: Color = Color::from_u32(0x00111111);
+    const LIGHT_BACKGROUND: Color = Color::from_u32(0x00191917);
+    const FOREGROUND: Color = Color::from_u32(0x00fafafa);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x00a1a1aa);
+    const COLORS: [Color; 6] = [
+        Color::from_u32(0x00f472b6),
+        Color::from_u32(0x00c084fc),
+        Color::from_u32(0x0022d3ee),
+        Color::from_u32(0x0060a5fa),
+        Color::from_u32(0x002dd4bf),
+        Color::from_u32(0x004ade80),
+    ];
+    const DARK_COLORS: [Color; 6] = [
+        Color::from_u32(0x00ec4899),
+        Color::from_u32(0x00a855f7),
+        Color::from_u32(0x0006b6d4),
+        Color::from_u32(0x003b82f6),
+        Color::from_u32(0x0014b8a6),
+        Color::from_u32(0x0022c55e),
+    ];
+    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x0018181b), Color::from_u32(0x00111111)];
+    const HIGHTLIGHT_BACKGROUND: Color = Color::from_u32(0x00ca8a04);
+    const HIGHTLIGHT_FOREGROUND: Color = Color::from_u32(0x00fafafa);
+    const STATUS_BAR_ERROR: Color = Color::from_u32(0x00991919);
 }
 
 impl Styler for Terminal {
