003fcb1f4456da87a942a187ace12ead0283e8ab
Shayan Hashemi <shayan.hashemi@gmail.com>
2025-08-06 00:14:47 +0300

    use scroll view for histogram

--- src/tui/histogram_plot.rs
+++ src/tui/histogram_plot.rs
@@ -1,11 +1,10 @@
-use std::ops::Add;
-
 use itertools::Itertools;
 use ratatui::{
-    layout::{Alignment, Direction},
+    layout::{Alignment, Direction, Size},
     text::Line,
     widgets::{Bar, BarChart, BarGroup, Block, BorderType, Clear, StatefulWidget, Widget},
 };
+use tui_scrollview::{ScrollView, ScrollViewState, ScrollbarVisibility};
 
 use crate::misc::globals::theme;
 
@@ -15,12 +14,15 @@ pub struct HistogramPlot;
 #[derive(Debug)]
 pub struct HistogramPlotState {
     data: Vec<(String, u64)>,
-    scroll: usize,
+    scroll_view: ScrollViewState,
 }
 
 impl HistogramPlotState {
     pub fn new(data: Vec<(String, u64)>) -> Self {
-        Self { data, scroll: 0 }
+        Self {
+            data,
+            scroll_view: Default::default(),
+        }
     }
 
     pub fn bucket_count(&self) -> usize {
@@ -28,13 +30,11 @@ impl HistogramPlotState {
     }
 
     pub fn scroll_up(&mut self) {
-        //
-        self.scroll = self.scroll.saturating_sub(1);
+        self.scroll_view.scroll_up();
     }
 
     pub fn scroll_down(&mut self) {
-        //
-        self.scroll = self.scroll.add(1);
+        self.scroll_view.scroll_down();
     }
 }
 
@@ -48,39 +48,56 @@ impl StatefulWidget for HistogramPlot {
         state: &mut Self::State,
     ) {
         Widget::render(Clear, area, buf);
+        let area = {
+            let blk = Block::bordered()
+                .border_type(BorderType::Rounded)
+                .style(theme().block())
+                .title("Histogram Plot")
+                .title_alignment(Alignment::Center);
+            let new_area = blk.inner(area);
+            blk.render(area, buf);
+            new_area
+        };
 
-        let height = area.height.saturating_sub(2).into();
-        state.scroll = state.scroll.min(state.data.len().saturating_sub(height));
-
+        let lab_len = state
+            .data
+            .iter()
+            .map(|(l, _)| l.len())
+            .max()
+            .unwrap_or_default();
+        let val_len = state
+            .data
+            .iter()
+            .map(|(_, v)| v.to_string().len())
+            .max()
+            .unwrap_or_default();
         let bars = state
             .data
             .iter()
-            .skip(state.scroll)
-            .take(height)
             .enumerate()
             .map(|(idx, (label, value))| {
                 Bar::default()
                     .value(*value)
-                    // .label(Line::raw(label).style(theme().text()))
-                    .label(Line::styled(label, theme().graph(idx)))
+                    .text_value(format!("{value:>val_len$} "))
+                    .label(Line::styled(
+                        format!("{label:>lab_len$} "),
+                        theme().graph(idx),
+                    ))
                     .style(theme().graph(idx))
             })
             .collect_vec();
 
+        let mut scroll_view = ScrollView::new(Size::new(area.width, bars.len() as u16))
+            .scrollbars_visibility(ScrollbarVisibility::Never);
         if !bars.is_empty() {
             let chart = BarChart::default()
-                .block(
-                    Block::bordered()
-                        .border_type(BorderType::Rounded)
-                        .style(theme().block())
-                        .title("Histogram Plot")
-                        .title_alignment(Alignment::Center),
-                )
+                .style(theme().text())
                 .bar_width(1)
                 .direction(Direction::Horizontal)
                 .bar_gap(0)
                 .data(BarGroup::default().bars(&bars));
-            chart.render(area, buf);
+            scroll_view.render_widget(chart, scroll_view.area());
         }
+        scroll_view.render(area, buf, &mut state.scroll_view);
     }
 }
