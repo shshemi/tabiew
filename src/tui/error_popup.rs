3a14681e3c6a3c4915dae96047311e96864b83df
shshemi <shshemi@gmail.com>
2025-09-13 10:44:29 +0300

    remove line_count

--- src/tui/error_popup.rs
+++ src/tui/error_popup.rs
@@ -1,51 +1,51 @@
-use std::borrow::Cow;
-
 use ratatui::{
-    layout::Alignment,
-    widgets::{Block, Clear, Paragraph, Widget, Wrap},
+    layout::{Alignment, Constraint, Flex, Layout},
+    text::Text,
+    widgets::{Clear, Paragraph, Widget, Wrap},
 };
 
-use crate::misc::globals::theme;
-
-use super::utils::line_count;
+use crate::{misc::globals::theme, tui::widgets::block::Block};
 
 #[derive(Debug, Default)]
 pub struct ErrorPopup<'a> {
-    message: Cow<'a, str>,
+    pg: Paragraph<'a>,
 }
 
 impl<'a> ErrorPopup<'a> {
     pub fn new() -> Self {
         Self {
-            message: Default::default(),
+            pg: Default::default(),
         }
     }
-    pub fn with_message(mut self, message: impl Into<Cow<'a, str>>) -> Self {
-        self.message = message.into();
+    pub fn with_message(mut self, message: impl Into<Text<'a>>) -> Self {
+        self.pg = Paragraph::new(message)
+            .left_aligned()
+            .block(
+                Block::default()
+                    .title(" Error ")
+                    .title_alignment(Alignment::Center)
+                    .style(theme().error())
+                    .into_widget(),
+            )
+            .wrap(Wrap { trim: true });
         self
-    }
-
-    pub fn line_count(&self, width: usize) -> u16 {
-        line_count(self.message.as_ref(), width - 2) as u16 + 2
     }
 }
 
 impl Widget for ErrorPopup<'_> {
-    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+    fn render(self, _: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
     where
         Self: Sized,
     {
+        let text_width = self.pg.line_width().min(64) as u16;
+        let [area] = Layout::horizontal([Constraint::Length(text_width)])
+            .flex(Flex::Center)
+            .areas(buf.area);
+        let [area] =
+            Layout::vertical([Constraint::Length((self.pg.line_count(text_width)) as u16)])
+                .flex(Flex::Center)
+                .areas(area);
         Clear.render(area, buf);
-        Paragraph::new(self.message)
-            .left_aligned()
-            .block(
-                Block::bordered()
-                    .title(" Error ")
-                    .title_alignment(Alignment::Center)
-                    .border_style(theme().error()),
-            )
-            .style(theme().error())
-            .wrap(Wrap { trim: true })
-            .render(area, buf);
+        self.pg.render(area, buf);
     }
 }
