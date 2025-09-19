399c554995d792c7e6a683b356db8954b43e2c5f
shshemi <shshemi@gmail.com>
2025-09-19 19:00:40 +0300

    add config

--- src/misc/config.rs
+++ src/misc/config.rs
@@ -1,24 +1,42 @@
-use serde::Serialize;
+use std::{
+    ops::{Deref, DerefMut},
+    sync::RwLock,
+};
 
-use crate::tui::{Styler, theme::Monokai};
+use serde::{Deserialize, Serialize};
 
+use crate::{AppResult, tui::theme::Theme};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(default)]
 pub struct Config {
-    theme: Box<dyn Styler>,
+    theme: RwLock<Theme>,
+}
+
+impl Config {
+    pub const fn new() -> Self {
+        Self {
+            theme: RwLock::new(Theme::default()),
+        }
+    }
+
+    pub fn load(&self, text: &str) -> AppResult<()> {
+        let Config { theme } = toml::from_str(text)?;
+        *self.theme_mut() = theme.into_inner().unwrap();
+        Ok(())
+    }
+
+    pub fn theme(&self) -> impl Deref<Target = Theme> {
+        self.theme.read().unwrap()
+    }
+
+    pub fn theme_mut(&self) -> impl DerefMut<Target = Theme> {
+        self.theme.write().unwrap()
+    }
 }
 
 impl Default for Config {
     fn default() -> Self {
-        Self {
-            theme: Box::new(Monokai::default()),
-        }
-    }
-}
-
-impl Serialize for Config {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        todo!()
+        Self::new()
     }
 }
--- src/misc/mod.rs
+++ src/misc/mod.rs
@@ -1,3 +1,4 @@
+pub mod config;
 pub mod globals;
 pub mod history;
 pub mod iter_ext;
