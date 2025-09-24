fb818bbfc69c109f8cb2f9acba029a6f7b6b685f
shshemi <shshemi@gmail.com>
2025-09-25 01:48:10 +0300

    make search picker based on sebsequence

--- src/misc/type_ext.rs
+++ src/misc/type_ext.rs
@@ -20,7 +20,7 @@ impl ToAscii for char {
 }
 
 pub trait HasSubsequence {
-    fn has_subsequence(&self, other: &Self, max_space: usize) -> bool;
+    fn has_subsequence(&self, other: &Self) -> bool;
 }
 
 pub trait UnwrapOrGracefulShutdown<T> {
@@ -28,22 +28,13 @@ pub trait UnwrapOrGracefulShutdown<T> {
 }
 
 impl HasSubsequence for str {
-    fn has_subsequence(&self, other: &Self, max_space: usize) -> bool {
+    fn has_subsequence(&self, other: &Self) -> bool {
         let mut oitr = other.chars();
         let mut current = oitr.next();
-        let mut space = 0;
-        let mut first_match = false;
         for chr in self.chars() {
             if let Some(cur) = current {
                 if chr == cur {
                     current = oitr.next();
-                    space = 0;
-                    first_match = true;
-                } else if first_match {
-                    space += 1;
-                }
-                if space > max_space {
-                    break;
                 }
             } else {
                 break;
@@ -167,52 +158,53 @@ mod tests {
 
     #[test]
     fn test_has_subsequence() {
-        assert!("abcdef".has_subsequence("abc", 10));
-        assert!("abcdef".has_subsequence("ace", 2));
-        assert!(!"abcdef".has_subsequence("aec", 2));
-        assert!("".has_subsequence("", 10));
-        assert!("abcdef".has_subsequence("", 10));
-        assert!(!"".has_subsequence("abc", 10));
-        assert!("a".has_subsequence("a", 0));
-        assert!(!"a".has_subsequence("b", 0));
-        assert!("abcdef".has_subsequence("a", 10));
-        assert!("abcdef".has_subsequence("abcdef", 10));
-        assert!(!"abcdef".has_subsequence("abcdefg", 10));
-        assert!("abcdef".has_subsequence("ace", 2));
-        assert!("abcdef".has_subsequence("ace", 1));
-        assert!("abcdef".has_subsequence("acf", 3));
-        assert!("abcdef".has_subsequence("abc", 0));
-        assert!(!"abcdef".has_subsequence("ace", 0));
-        assert!("abcdef".has_subsequence("ace", 10));
-        assert!("abcdef".has_subsequence("f", 100));
-        assert!("aaaaa".has_subsequence("aaa", 1));
-        assert!("aaaaa".has_subsequence("aaaa", 0));
-        assert!("a".has_subsequence("a", 0));
-        assert!(!"abcdef".has_subsequence("g", 10));
-        assert!("abcdef".has_subsequence("a", 0));
-        assert!("abcdef".has_subsequence("f", 0));
-        assert!("abcdef".has_subsequence("d", 2));
-        assert!(!"".has_subsequence("a", 0));
-        assert!("abc".has_subsequence("b", 0));
-        assert!("abc".has_subsequence("b", 1));
-        assert!("abc".has_subsequence("b", 2));
-        assert!("abc".has_subsequence("c", 2));
-        assert!(!"abc".has_subsequence("d", 0));
-        assert!(!"abacabad".has_subsequence("aad", 3));
-        assert!(!"abacabad".has_subsequence("aad", 2));
-        assert!("x".has_subsequence("x", 0));
-        assert!(!"x".has_subsequence("y", 0));
-        assert!("aaaaa".has_subsequence("aaaaa", 0));
-        assert!(!"aaaaa".has_subsequence("aaaaaa", 0));
-        assert!("abcdef".has_subsequence("abcdef", 5));
-        assert!(!"abcdef".has_subsequence("abcdefg", 5));
-        assert!("a b c d e f".has_subsequence("abcdef", 5));
-        assert!(!"abcdef".has_subsequence("a b c", 0));
-        assert!("abacabadabacaba".has_subsequence("ababa", 5));
-        assert!("abacabadabacaba".has_subsequence("ababa", 4));
-        assert!("xyxyxyxyxy".has_subsequence("yyy", 3));
-        assert!("xyxyxyxyxy".has_subsequence("yyy", 1));
-        assert!(!"xyxyxyxyxy".has_subsequence("yyy", 0));
+        assert!("Chakra".has_subsequence("Ca"));
+        assert!("abcdef".has_subsequence("abc"));
+        assert!("abcdef".has_subsequence("ace"));
+        assert!(!"abcdef".has_subsequence("aec"));
+        assert!("".has_subsequence(""));
+        assert!("abcdef".has_subsequence(""));
+        assert!(!"".has_subsequence("abc"));
+        assert!("a".has_subsequence("a"));
+        assert!(!"a".has_subsequence("b"));
+        assert!("abcdef".has_subsequence("a"));
+        assert!("abcdef".has_subsequence("abcdef"));
+        assert!(!"abcdef".has_subsequence("abcdefg"));
+        assert!("abcdef".has_subsequence("ace"));
+        assert!("abcdef".has_subsequence("ace"));
+        assert!("abcdef".has_subsequence("acf"));
+        assert!("abcdef".has_subsequence("abc"));
+        assert!(!"abcdef".has_subsequence("ace"));
+        assert!("abcdef".has_subsequence("ace"));
+        assert!("abcdef".has_subsequence("f"));
+        assert!("aaaaa".has_subsequence("aaa"));
+        assert!("aaaaa".has_subsequence("aaaa"));
+        assert!("a".has_subsequence("a"));
+        assert!(!"abcdef".has_subsequence("g"));
+        assert!("abcdef".has_subsequence("a"));
+        assert!("abcdef".has_subsequence("f"));
+        assert!("abcdef".has_subsequence("d"));
+        assert!(!"".has_subsequence("a"));
+        assert!("abc".has_subsequence("b"));
+        assert!("abc".has_subsequence("b"));
+        assert!("abc".has_subsequence("b"));
+        assert!("abc".has_subsequence("c"));
+        assert!(!"abc".has_subsequence("d"));
+        assert!(!"abacabad".has_subsequence("aad"));
+        assert!(!"abacabad".has_subsequence("aad"));
+        assert!("x".has_subsequence("x"));
+        assert!(!"x".has_subsequence("y"));
+        assert!("aaaaa".has_subsequence("aaaaa"));
+        assert!(!"aaaaa".has_subsequence("aaaaaa"));
+        assert!("abcdef".has_subsequence("abcdef"));
+        assert!(!"abcdef".has_subsequence("abcdefg"));
+        assert!("a b c d e f".has_subsequence("abcdef"));
+        assert!(!"abcdef".has_subsequence("a b c"));
+        assert!("abacabadabacaba".has_subsequence("ababa"));
+        assert!("abacabadabacaba".has_subsequence("ababa"));
+        assert!("xyxyxyxyxy".has_subsequence("yyy"));
+        assert!("xyxyxyxyxy".has_subsequence("yyy"));
+        assert!(!"xyxyxyxyxy".has_subsequence("yyy"));
     }
 
     #[test]
