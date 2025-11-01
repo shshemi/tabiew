f5457e52482a8afa341dd5b94d4b4c3e8d05ba60
shshemi <shshemi@gmail.com>
2025-11-01 14:44:09 +0000

    add path picker

---
+++ src/tui/popups/path_picker.rs
@@ -0,0 +1,48 @@
+use std::borrow::Cow;
+
+use ratatui::widgets::StatefulWidget;
+
+use crate::tui::pickers::text_picker::{TextPicker, TextPickerState};
+
+#[derive(Debug, Default)]
+pub struct PathPickerState {
+    text_picker: TextPickerState,
+}
+
+#[derive(Debug, Default)]
+pub struct PathPicker<'a> {
+    title: Option<Cow<'a, str>>,
+    hint: Option<Cow<'a, str>>,
+}
+
+impl<'a> PathPicker<'a> {
+    pub fn with_title(self, title: impl Into<Cow<'a, str>>) -> Self {
+        Self {
+            title: Some(title.into()),
+            ..self
+        }
+    }
+
+    pub fn with_hint(self, hint: impl Into<Cow<'a, str>>) -> Self {
+        Self {
+            hint: Some(hint.into()),
+            ..self
+        }
+    }
+}
+
+impl<'a> StatefulWidget for PathPicker<'a> {
+    type State = PathPickerState;
+
+    fn render(
+        self,
+        area: ratatui::prelude::Rect,
+        buf: &mut ratatui::prelude::Buffer,
+        state: &mut Self::State,
+    ) {
+        TextPicker::default()
+            .title(self.title.unwrap_or(Cow::Borrowed("Output Path")))
+            .hint(self.hint.unwrap_or_default())
+            .render(area, buf, &mut state.text_picker);
+    }
+}
