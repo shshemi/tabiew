5f0c6d3d3e82084948f9a0ab4f74e98e873697de
shshemi <shshemi@gmail.com>
2025-11-04 16:34:31 +0000

    add parquet_exporter for wizard

---
+++ src/tui/popups/exporters/parquet_exporter.rs
@@ -0,0 +1,67 @@
+use std::path::PathBuf;
+
+use crossterm::event::KeyEvent;
+use ratatui::widgets::StatefulWidget;
+
+use crate::tui::popups::path_picker::{PathPicker, PathPickerState};
+
+#[derive(Debug)]
+pub enum State {
+    PickOutputPath { picker: PathPickerState },
+    ExportToFile { path: PathBuf },
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self::PickOutputPath {
+            picker: Default::default(),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct ParquetExporterState {
+    inner: State,
+}
+
+impl ParquetExporterState {
+    pub fn step(&mut self) -> &State {
+        self.inner = match std::mem::take(&mut self.inner) {
+            State::PickOutputPath { picker } => State::ExportToFile {
+                path: picker.path(),
+            },
+            State::ExportToFile { path } => State::ExportToFile { path },
+        };
+        &self.inner
+    }
+
+    pub fn inner(&self) -> &State {
+        &self.inner
+    }
+
+    pub fn handle(&mut self, event: KeyEvent) {
+        match &mut self.inner {
+            State::PickOutputPath { picker } => picker.handle(event),
+            _ => (),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct ParquetExporter {}
+
+impl StatefulWidget for ParquetExporter {
+    type State = ParquetExporterState;
+
+    fn render(
+        self,
+        area: ratatui::prelude::Rect,
+        buf: &mut ratatui::prelude::Buffer,
+        state: &mut Self::State,
+    ) {
+        match &mut state.inner {
+            State::PickOutputPath { picker } => PathPicker::default().render(area, buf, picker),
+            State::ExportToFile { path: _ } => (),
+        }
+    }
+}
