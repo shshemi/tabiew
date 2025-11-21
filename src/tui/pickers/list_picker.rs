479bc719f2d25beed2e10370316390d175d7db1c
shshemi <shshemi@gmail.com>
2025-11-21 04:09:49 +0000

    make list picker a component

--- src/tui/pickers/list_picker.rs
+++ src/tui/pickers/list_picker.rs
@@ -1,17 +1,30 @@
+use crossterm::event::KeyCode;
 use ratatui::{
     layout::{Constraint, Flex, Layout},
-    text::Line,
-    widgets::{Clear, List, ListItem, ListState, StatefulWidget, Widget, block::Title},
+    widgets::{Clear, List, ListItem, ListState, StatefulWidget, Widget},
 };
 
-use crate::{misc::globals::theme, tui::widgets::block::Block};
+use crate::{
+    misc::globals::theme,
+    tui::{component::Component, widgets::block::Block},
+};
 
 #[derive(Debug)]
 pub struct ListPickerState {
+    title: String,
     list: ListState,
+    items: Vec<String>,
 }
 
 impl ListPickerState {
+    pub fn new(items: Vec<String>) -> Self {
+        Self {
+            list: ListState::default().with_selected(0.into()),
+            items,
+            title: Default::default(),
+        }
+    }
+
     pub fn list(&self) -> &ListState {
         &self.list
     }
@@ -21,54 +34,12 @@ impl ListPickerState {
     }
 }
 
-impl Default for ListPickerState {
-    fn default() -> Self {
-        Self {
-            list: ListState::default().with_selected(0.into()),
-        }
-    }
-}
-
-#[derive(Debug, Default)]
-pub struct ListPicker<'a> {
-    items: Vec<ListItem<'a>>,
-    block: Block<'a>,
-    width: u16,
-}
-
-impl<'a> ListPicker<'a> {
-    pub fn title<T: Into<Title<'a>>>(mut self, title: T) -> Self {
-        self.block = self.block.title(title);
-        self
-    }
-
-    pub fn bottom<T: Into<Line<'a>>>(mut self, title: T) -> Self {
-        self.block = self.block.bottom(title);
-        self
-    }
-
-    pub fn items<T>(mut self, items: T) -> Self
-    where
-        T: IntoIterator,
-        T::Item: Into<ListItem<'a>>,
-    {
-        self.items = items
-            .into_iter()
-            .map(Into::into)
-            .inspect(|d| self.width = self.width.max(d.width() as u16))
-            .collect();
-        self
-    }
-}
-
-impl<'a> StatefulWidget for ListPicker<'a> {
-    type State = ListPickerState;
-
+impl Component for ListPickerState {
     fn render(
-        self,
-        _: ratatui::prelude::Rect,
+        &mut self,
+        _area: ratatui::prelude::Rect,
         buf: &mut ratatui::prelude::Buffer,
-        state: &mut Self::State,
+        _focus_state: crate::tui::component::FocusState,
     ) {
         let width = 80;
         let height = self.items.len().saturating_add(2).min(25) as u16;
@@ -84,11 +55,89 @@ impl<'a> StatefulWidget for ListPicker<'a> {
             List::default()
                 .style(theme().text())
                 .highlight_style(theme().row_highlighted())
-                .items(self.items)
-                .block(self.block.into_widget()),
+                .items(self.items.iter().map(|s| ListItem::from(s.as_str())))
+                .block(Block::default().title(self.title.as_str()).into_widget()),
             area,
             buf,
-            &mut state.list,
+            &mut self.list,
         );
     }
+
+    fn handle(&mut self, event: crossterm::event::KeyEvent) -> bool {
+        match event.code {
+            KeyCode::Up => {
+                self.list.select_previous();
+                true
+            }
+            KeyCode::Down => {
+                self.list.select_next();
+                true
+            }
+            _ => false,
+        }
+    }
 }
+
+// #[derive(Debug, Default)]
+// pub struct ListPicker<'a> {
+//     items: Vec<ListItem<'a>>,
+//     block: Block<'a>,
+//     width: u16,
+// }
+
+// impl<'a> ListPicker<'a> {
+//     pub fn title<T: Into<Title<'a>>>(mut self, title: T) -> Self {
+//         self.block = self.block.title(title);
+//         self
+//     }
+
+//     pub fn bottom<T: Into<Line<'a>>>(mut self, title: T) -> Self {
+//         self.block = self.block.bottom(title);
+//         self
+//     }
+
+//     pub fn items<T>(mut self, items: T) -> Self
+//     where
+//         T: IntoIterator,
+//         T::Item: Into<ListItem<'a>>,
+//     {
+//         self.items = items
+//             .into_iter()
+//             .map(Into::into)
+//             .inspect(|d| self.width = self.width.max(d.width() as u16))
+//             .collect();
+//         self
+//     }
+// }
+
+// impl<'a> StatefulWidget for ListPicker<'a> {
+//     type State = ListPickerState;
+
+//     fn render(
+//         self,
+//         _: ratatui::prelude::Rect,
+//         buf: &mut ratatui::prelude::Buffer,
+//         state: &mut Self::State,
+//     ) {
+//         let width = 80;
+//         let height = self.items.len().saturating_add(2).min(25) as u16;
+
+//         let [area] = Layout::horizontal([Constraint::Length(width)])
+//             .flex(Flex::Center)
+//             .areas(buf.area);
+//         let [_, area] =
+//             Layout::vertical([Constraint::Length(3), Constraint::Length(height)]).areas(area);
+//         Clear.render(area, buf);
+
+//         StatefulWidget::render(
+//             List::default()
+//                 .style(theme().text())
+//                 .highlight_style(theme().row_highlighted())
+//                 .items(self.items)
+//                 .block(self.block.into_widget()),
+//             area,
+//             buf,
+//             &mut state.list,
+//         );
+//     }
+// }
