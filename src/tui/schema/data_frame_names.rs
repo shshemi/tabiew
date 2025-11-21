bf641dd9ae6435ad831f48feccd32d67ca6b77bc
shshemi <shshemi@gmail.com>
2025-11-21 22:51:28 +0000

    make data frame names component

--- src/tui/schema/data_frame_names.rs
+++ src/tui/schema/data_frame_names.rs
@@ -1,5 +1,3 @@
-use std::borrow::Cow;
-
 use ratatui::{
     layout::{Alignment, Constraint},
     text::Span,
@@ -7,27 +5,24 @@ use ratatui::{
 };
 
 use crate::{
-    misc::globals::theme,
+    misc::globals::{sql, theme},
     tui::{
+        component::Component,
         status_bar::{StatusBar, Tag},
         widgets::block::Block,
     },
 };
 
 #[derive(Debug)]
-pub struct DataFrameNamesState {
+pub struct DataFrameNames {
     table: TableState,
+    items: Vec<String>,
 }
 
-impl Default for DataFrameNamesState {
-    fn default() -> Self {
-        Self {
-            table: TableState::default().with_selected(0),
-        }
+impl DataFrameNames {
+    pub fn selected(&self) -> Option<usize> {
+        self.table.selected()
     }
-}
-
-impl DataFrameNamesState {
     pub fn table(&self) -> &TableState {
         &self.table
     }
@@ -36,38 +31,22 @@ impl DataFrameNamesState {
         &mut self.table
     }
 }
-pub struct DataFrameNames<I> {
-    names: I,
-}
 
-impl<T> DataFrameNames<T> {
-    pub fn new(names: T) -> Self {
-        DataFrameNames { names }
-    }
-}
-
-impl<'a, I> StatefulWidget for DataFrameNames<I>
-where
-    I: IntoIterator,
-    I::Item: Into<Cow<'a, str>>,
-{
-    type State = DataFrameNamesState;
-
+impl Component for DataFrameNames {
     fn render(
-        self,
+        &mut self,
         area: ratatui::prelude::Rect,
         buf: &mut ratatui::prelude::Buffer,
-        state: &mut Self::State,
+        _focus_state: crate::tui::component::FocusState,
     ) {
-        let items = self.names.into_iter().collect::<Vec<_>>();
-        let num_width = items.len().to_string().len();
+        let num_width = self.items.len().to_string().len();
 
         Table::default()
-            .rows(items.into_iter().enumerate().map(|(i, s)| {
+            .rows(self.items.iter().enumerate().map(|(i, s)| {
                 Row::new([
                     Span::raw(format!(" {:>width$}", i + 1, width = num_width))
                         .style(theme().subtext()),
-                    Span::raw(s.into()).style(theme().text()),
+                    Span::raw(s).style(theme().text()),
                 ])
             }))
             .row_highlight_style(theme().row_highlighted())
@@ -89,6 +68,75 @@ where
                     .title_alignment(Alignment::Center)
                     .into_widget(),
             )
-            .render(area, buf, &mut state.table);
+            .render(area, buf, &mut self.table);
     }
 }
+
+impl Default for DataFrameNames {
+    fn default() -> Self {
+        Self {
+            table: TableState::default().with_selected(0),
+            items: sql()
+                .schema()
+                .iter()
+                .map(|(name, _)| name.to_owned())
+                .collect(),
+        }
+    }
+}
+// pub struct DataFrameNames<I> {
+//     names: I,
+// }
+
+// impl<T> DataFrameNames<T> {
+//     pub fn new(names: T) -> Self {
+//         DataFrameNames { names }
+//     }
+// }
+
+// impl<'a, I> StatefulWidget for DataFrameNames<I>
+// where
+//     I: IntoIterator,
+//     I::Item: Into<Cow<'a, str>>,
+// {
+//     type State = DataFrameNamesState;
+
+//     fn render(
+//         self,
+//         area: ratatui::prelude::Rect,
+//         buf: &mut ratatui::prelude::Buffer,
+//         state: &mut Self::State,
+//     ) {
+//         let items = self.names.into_iter().collect::<Vec<_>>();
+//         let num_width = items.len().to_string().len();
+
+//         Table::default()
+//             .rows(items.into_iter().enumerate().map(|(i, s)| {
+//                 Row::new([
+//                     Span::raw(format!(" {:>width$}", i + 1, width = num_width))
+//                         .style(theme().subtext()),
+//                     Span::raw(s.into()).style(theme().text()),
+//                 ])
+//             }))
+//             .row_highlight_style(theme().row_highlighted())
+//             .widths([
+//                 Constraint::Length(num_width as u16 + 1),
+//                 Constraint::Fill(1),
+//             ])
+//             .column_spacing(1)
+//             .block(
+//                 Block::default()
+//                     .title("Tables")
+//                     .bottom(
+//                         StatusBar::new()
+//                             .mono_color()
+//                             .centered()
+//                             .tag(Tag::new(" Open ", " Enter"))
+//                             .tag(Tag::new(" Unload ", " Delete ")),
+//                     )
+//                     .title_alignment(Alignment::Center)
+//                     .into_widget(),
+//             )
+//             .render(area, buf, &mut state.table);
+//     }
+// }
