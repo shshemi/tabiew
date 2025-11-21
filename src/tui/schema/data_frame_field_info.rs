2d98421c1349b88edc5bc4cb80e34f39ff98becd
shshemi <shshemi@gmail.com>
2025-11-21 22:52:14 +0000

    make data frame field component

--- src/tui/schema/data_frame_field_info.rs
+++ src/tui/schema/data_frame_field_info.rs
@@ -1,3 +1,4 @@
+use crossterm::event::KeyCode;
 use ratatui::{
     layout::{Alignment, Constraint},
     text::Text,
@@ -7,17 +8,26 @@ use ratatui::{
 use crate::{
     misc::{globals::theme, sql::TableSchema, type_ext::human_readable_size},
     tui::{
+        component::Component,
         status_bar::{StatusBar, Tag},
         widgets::block::Block,
     },
 };
 
-#[derive(Debug, Default)]
-pub struct DataFrameFieldInfoState {
+#[derive(Debug)]
+pub struct DataFrameFieldInfo {
     table_state: TableState,
+    table_schema: TableSchema,
 }
 
-impl DataFrameFieldInfoState {
+impl DataFrameFieldInfo {
+    pub fn new(table_schema: TableSchema) -> Self {
+        Self {
+            table_state: Default::default(),
+            table_schema,
+        }
+    }
+
     pub fn table_state(&self) -> &TableState {
         &self.table_state
     }
@@ -35,28 +45,14 @@ impl DataFrameFieldInfoState {
     }
 }
 
-pub struct DataFrameFieldInfo<'a> {
-    table_schema: &'a TableSchema,
-}
-
-impl<'a> DataFrameFieldInfo<'a> {
-    pub fn new(field_info: &'a TableSchema) -> Self {
-        Self {
-            table_schema: field_info,
-        }
-    }
-}
-
-impl StatefulWidget for DataFrameFieldInfo<'_> {
-    type State = DataFrameFieldInfoState;
-
+impl Component for DataFrameFieldInfo {
     fn render(
-        self,
+        &mut self,
         area: ratatui::prelude::Rect,
         buf: &mut ratatui::prelude::Buffer,
-        state: &mut Self::State,
+        focus_state: crate::tui::component::FocusState,
     ) {
-        *state.table_state.offset_mut() = state.table_state.offset().min(
+        *self.table_state.offset_mut() = self.table_state.offset().min(
             self.table_schema
                 .len()
                 .saturating_sub(area.height.saturating_sub(2).into()),
@@ -113,7 +109,104 @@ impl StatefulWidget for DataFrameFieldInfo<'_> {
                 ),
             area,
             buf,
-            &mut state.table_state,
+            &mut self.table_state,
         );
     }
+
+    fn handle(&mut self, event: crossterm::event::KeyEvent) -> bool {
+        match event.code {
+            KeyCode::Char('K') => {
+                self.scroll_up();
+                true
+            }
+            KeyCode::Char('J') => {
+                self.scroll_down();
+                true
+            }
+            _ => false,
+        }
+    }
 }
+
+// pub struct DataFrameFieldInfo<'a> {
+//     table_schema: &'a TableSchema,
+// }
+
+// impl<'a> DataFrameFieldInfo<'a> {
+//     pub fn new(field_info: &'a TableSchema) -> Self {
+//         Self {
+//             table_schema: field_info,
+//         }
+//     }
+// }
+
+// impl StatefulWidget for DataFrameFieldInfo<'_> {
+//     type State = DataFrameFieldInfoState;
+
+//     fn render(
+//         self,
+//         area: ratatui::prelude::Rect,
+//         buf: &mut ratatui::prelude::Buffer,
+//         state: &mut Self::State,
+//     ) {
+//         *state.table_state.offset_mut() = state.table_state.offset().min(
+//             self.table_schema
+//                 .len()
+//                 .saturating_sub(area.height.saturating_sub(2).into()),
+//         );
+//         Widget::render(Clear, area, buf);
+//         StatefulWidget::render(
+//             Table::default()
+//                 .header(
+//                     Row::new(
+//                         ["Name", "Type", "Estimated Size", "Null Count", "Min", "Max"]
+//                             .into_iter()
+//                             .enumerate()
+//                             .map(|(i, s)| Text::styled(s, theme().header(i))),
+//                     )
+//                     .style(theme().table_header()),
+//                 )
+//                 .rows(
+//                     self.table_schema
+//                         .iter()
+//                         .enumerate()
+//                         .map(|(idx, (name, info))| {
+//                             Row::new([
+//                                 name.to_owned(),
+//                                 format!("{}", info.dtype()),
+//                                 human_readable_size(info.estimated_size() as u64),
+//                                 format!("{}", info.null_count()),
+//                                 info.min().to_string(),
+//                                 info.max().to_string(),
+//                             ])
+//                             .style(theme().row(idx))
+//                         }),
+//                 )
+//                 .widths([
+//                     Constraint::Fill(1),
+//                     Constraint::Fill(1),
+//                     Constraint::Fill(1),
+//                     Constraint::Fill(1),
+//                     Constraint::Fill(1),
+//                     Constraint::Fill(1),
+//                 ])
+//                 .style(theme().text())
+//                 .block(
+//                     Block::default()
+//                         .borders(Borders::BOTTOM | Borders::RIGHT | Borders::LEFT)
+//                         .bottom(
+//                             StatusBar::new()
+//                                 .mono_color()
+//                                 .centered()
+//                                 .tag(Tag::new(" Scroll Up ", " Shift+K | Shift+\u{2191} "))
+//                                 .tag(Tag::new(" Scroll Down ", " Shift+J | Shift+\u{2193} ")),
+//                         )
+//                         .title_alignment(Alignment::Center)
+//                         .into_widget(),
+//                 ),
+//             area,
+//             buf,
+//             &mut state.table_state,
+//         );
+//     }
+// }
