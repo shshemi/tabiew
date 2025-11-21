d3131df15d25daa4df596e57f951227f50f1c7c8
shshemi <shshemi@gmail.com>
2025-11-21 22:51:57 +0000

    make dataframe metada data compoent

--- src/tui/schema/data_frame_meta_info.rs
+++ src/tui/schema/data_frame_meta_info.rs
@@ -10,24 +10,27 @@ use ratatui::{
 
 use crate::{
     misc::{globals::theme, sql, type_ext::human_readable_size},
-    tui::widgets::block::Block,
+    tui::{component::Component, widgets::block::Block},
 };
 
-pub struct DataFrameMetaInfo<'a> {
-    info: &'a sql::TableInfo,
+#[derive(Debug)]
+pub struct DataFrameMetaInfo {
+    info: sql::TableInfo,
 }
 
-impl<'a> DataFrameMetaInfo<'a> {
-    pub fn new(info: &'a sql::TableInfo) -> Self {
+impl DataFrameMetaInfo {
+    pub fn new(info: sql::TableInfo) -> Self {
         Self { info }
     }
 }
 
-impl Widget for DataFrameMetaInfo<'_> {
-    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
-    where
-        Self: Sized,
-    {
+impl Component for DataFrameMetaInfo {
+    fn render(
+        &mut self,
+        area: ratatui::prelude::Rect,
+        buf: &mut ratatui::prelude::Buffer,
+        _focus_state: crate::tui::component::FocusState,
+    ) {
         Widget::render(Clear, area, buf);
         Table::default()
             .rows([
@@ -69,3 +72,50 @@ impl Widget for DataFrameMetaInfo<'_> {
             .render(area, buf);
     }
 }
+
+// impl Widget for DataFrameMetaInfo<'_> {
+//     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+//     where
+//         Self: Sized,
+//     {
+//         Widget::render(Clear, area, buf);
+//         Table::default()
+//             .rows([
+//                 Row::new([
+//                     Span::styled("Path", theme().header(0)),
+//                     Span::styled(self.info.source().display_path(), theme().text()),
+//                 ]),
+//                 Row::new([
+//                     Span::styled("Shape", theme().header(1)),
+//                     Span::styled(
+//                         format!("{} x {}", self.info.height(), self.info.width()),
+//                         theme().text(),
+//                     ),
+//                 ]),
+//                 Row::new([
+//                     Span::styled("Total Estimated Memory", theme().header(2)),
+//                     Span::styled(
+//                         human_readable_size(self.info.total_est_size() as u64),
+//                         theme().text(),
+//                     ),
+//                 ]),
+//                 Row::new([
+//                     Span::styled("Total Null Count", theme().header(3)),
+//                     Span::styled(self.info.total_null().to_string(), theme().text()),
+//                 ]),
+//             ])
+//             .widths([Constraint::Max(23), Constraint::Fill(1)])
+//             .block(
+//                 Block::default()
+//                     .border_set(Set {
+//                         bottom_left: VERTICAL_RIGHT,
+//                         bottom_right: VERTICAL_LEFT,
+//                         ..ROUNDED
+//                     })
+//                     .title_alignment(Alignment::Center)
+//                     .title("Info")
+//                     .into_widget(),
+//             )
+//             .render(area, buf);
+//     }
+// }
