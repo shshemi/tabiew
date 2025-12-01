45a27edc24820e04c4a2c904b32e6d539979fc58
shshemi <shshemi@gmail.com>
2025-12-01 23:52:16 +0000

    update inline_query

--- src/tui/popups/inline_query.rs
+++ src/tui/popups/inline_query.rs
@@ -1,24 +1,26 @@
-use crossterm::event::KeyEvent;
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use polars::frame::DataFrame;
 
-use crate::tui::{component::Component, pickers::text_picker::TextPicker};
+use crate::{
+    AppResult,
+    handler::action::Action,
+    misc::globals::sql,
+    tui::{component::Component, pickers::text_picker::TextPicker},
+};
 
 #[derive(Debug)]
 pub struct InlineQuery {
+    df: DataFrame,
     text_picker: TextPicker,
     query_type: InlineQueryType,
 }
 
 impl InlineQuery {
-    pub fn new(query_type: InlineQueryType) -> Self {
+    pub fn new(df: DataFrame, query_type: InlineQueryType) -> Self {
         Self {
-            text_picker: TextPicker::default().with_title(
-                match query_type {
-                    InlineQueryType::Filter => "Filter",
-                    InlineQueryType::Order => "Order",
-                }
-                .to_owned(),
-            ),
+            text_picker: TextPicker::default().with_title(query_type.title()),
             query_type,
+            df,
         }
     }
 
@@ -28,6 +30,20 @@ impl InlineQuery {
 
     pub fn query_type(&self) -> &InlineQueryType {
         &self.query_type
+    }
+
+    fn sql_query(&self, query: &str) -> AppResult<DataFrame> {
+        Ok(sql().execute(query, self.df.clone())?)
+    }
+
+    fn select(&self, select: &str) -> AppResult<DataFrame> {
+        self.sql_query(&format!("SELECT {select} FROM _"))
+    }
+    fn order(&self, order: &str) -> AppResult<DataFrame> {
+        self.sql_query(&format!("SELECT * FROM _ ORDER BY {order}"))
+    }
+    fn filter(&self, filter: &str) -> AppResult<DataFrame> {
+        self.sql_query(&format!("SELECT * FROM _ where {filter}"))
     }
 }
 
@@ -40,37 +56,48 @@ impl Component for InlineQuery {
     ) {
         self.text_picker.render(area, buf, focus_state);
     }
+
     fn handle(&mut self, event: KeyEvent) -> bool {
         self.text_picker.handle(event)
+            || match (event.code, event.modifiers) {
+                (KeyCode::Enter, KeyModifiers::NONE) => {
+                    let result = match self.query_type {
+                        InlineQueryType::Filter => self.filter(self.value()),
+                        InlineQueryType::Order => self.order(self.value()),
+                    };
+                    match result {
+                        Ok(df) => {
+                            Action::PaneSetDataFrame(df).enqueue();
+                            Action::PaneDismissModal.enqueue();
+                        }
+                        Err(err) => {
+                            Action::AppShowError(err.to_string()).enqueue();
+                            Action::PaneDismissModal.enqueue();
+                        }
+                    }
+                    true
+                }
+                (KeyCode::Esc, KeyModifiers::NONE) => {
+                    Action::PaneDismissModal.enqueue();
+                    true
+                }
+                _ => false,
+            }
     }
 }
-
-// #[derive(Debug, Default)]
-// pub struct InlineQuery<'a> {
-//     hint: &'a str,
-// }
-
-// impl<'a> StatefulWidget for InlineQuery<'a> {
-//     type State = InlineQueryState;
-
-//     fn render(
-//         self,
-//         area: ratatui::prelude::Rect,
-//         buf: &mut ratatui::prelude::Buffer,
-//         state: &mut Self::State,
-//     ) {
-//         TextPicker::default()
-//             .title(match state.query_type {
-//                 InlineQueryType::Filter => "Filter",
-//                 InlineQueryType::Order => "Order",
-//             })
-//             .hint(self.hint)
-//             .render(area, buf, &mut state.text_picker);
-//     }
-// }
 
 #[derive(Debug, Clone, Copy)]
 pub enum InlineQueryType {
     Filter,
     Order,
+}
+
+impl InlineQueryType {
+    fn title(&self) -> String {
+        match self {
+            InlineQueryType::Filter => "Filter",
+            InlineQueryType::Order => "Order",
+        }
+        .to_owned()
+    }
 }
