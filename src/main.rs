94ab4e96be8bb151558a7416d90d073a5131cee4
shshemi <shshemi@gmail.com>
2025-12-11 02:53:53 +0000

    experiment

--- src/main.rs
+++ src/main.rs
@@ -183,25 +183,23 @@ fn start_tui(tabs: Vec<(String, DataFrame)>, script: String, history: History) -
     while app.running() {
         tui.draw(&mut app)?;
 
-        if let Ok(event) = tui.events.next() {
-            match event {
-                Event::Tick => app.tick(),
-                Event::Key(key_event) => {
-                    #[cfg(target_os = "windows")]
-                    {
-                        use crossterm::event::KeyEventKind;
-                        if matches!(key_event.kind, KeyEventKind::Press | KeyEventKind::Repeat) {
-                            app.handle(key_event);
-                        }
-                    }
-                    #[cfg(not(target_os = "windows"))]
-                    {
+        match tui.events.next()? {
+            Event::Tick => app.tick(),
+            Event::Key(key_event) => {
+                #[cfg(target_os = "windows")]
+                {
+                    use crossterm::event::KeyEventKind;
+                    if matches!(key_event.kind, KeyEventKind::Press | KeyEventKind::Repeat) {
                         app.handle(key_event);
                     }
                 }
-                Event::Mouse(_) => {}
-                Event::Resize(_, _) => {}
+                #[cfg(not(target_os = "windows"))]
+                {
+                    app.handle(key_event);
+                }
             }
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
         }
 
         while let Ok(event) = tui.events.try_next() {
