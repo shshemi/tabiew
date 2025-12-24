5aba72dd845da31ec12a4199fd1d1f0bd13bf2b6
shshemi <shshemi@gmail.com>
2025-12-24 14:44:19 +0000

    add password to sqlite importer

--- src/tui/popups/importers/sqlite_importer.rs
+++ src/tui/popups/importers/sqlite_importer.rs
@@ -1,18 +1,37 @@
+use std::path::PathBuf;
+
 use crate::{
     reader::{Source, SqliteToDataFrames},
-    tui::popups::{importers::final_step, path_picker::PathPicker, wizard::WizardState},
+    tui::{
+        pickers::text_picker::TextPicker,
+        popups::{importers::final_step, path_picker::PathPicker, wizard::WizardState},
+    },
 };
 
 #[derive(Debug)]
 pub enum State {
     PickPath { picker: PathPicker },
+    PickPassword { path: PathBuf, picker: TextPicker },
 }
 
 impl WizardState for State {
     fn next(self) -> Self {
         match self {
-            State::PickPath { picker } => {
-                final_step(Source::File(picker.path()), SqliteToDataFrames::default());
+            State::PickPath { picker } => State::PickPassword {
+                path: picker.path(),
+                picker: TextPicker::default()
+                    .with_title("Password")
+                    .with_hint("Leave empty for no password"),
+            },
+            State::PickPassword { path, picker } => {
+                final_step(
+                    Source::File(path),
+                    if picker.value().is_empty() {
+                        SqliteToDataFrames::default()
+                    } else {
+                        SqliteToDataFrames::default().key(picker.value().to_owned())
+                    },
+                );
                 Default::default()
             }
         }
@@ -21,6 +40,7 @@ impl WizardState for State {
     fn responder(&mut self) -> &mut dyn crate::tui::component::Component {
         match self {
             State::PickPath { picker } => picker,
+            State::PickPassword { path: _, picker } => picker,
         }
     }
 }
