a51bcdc889ee155c15b157e12061d6a7b7ee35b0
shshemi <shshemi@gmail.com>
2025-12-26 23:40:13 +0000

    remove history path

--- src/misc/paths.rs
+++ src/misc/paths.rs
@@ -1,13 +1,9 @@
 use std::path::PathBuf;
 
-pub fn history_path() -> Option<PathBuf> {
-    home::home_dir().map(|path| path.join(".tabiew_history"))
-}
-
 pub fn theme_path() -> Option<PathBuf> {
     home::home_dir().map(|path| path.join(".config").join("tabiew").join("theme.toml"))
 }
 
-pub fn config_path() -> Option<PathBuf> {
+pub fn config_gath() -> Option<PathBuf> {
     home::home_dir().map(|path| path.join(".config").join("tabiew").join("config.toml"))
 }
