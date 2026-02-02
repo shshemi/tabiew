d5474d7572667acf4678b4644e14525ff5058d3c
shshemi <shshemi@gmail.com>
2026-02-02 01:44:40 +0000

    add sleep to thread to reduce cpu usage

--- src/handler/event.rs
+++ src/handler/event.rs
@@ -47,13 +47,8 @@ impl EventHandler {
                 let timeout = tick_rate
                     .checked_sub(last_tick.elapsed())
                     .unwrap_or(tick_rate);
-
-                {
-                    // let _lock = lock_event();
-
-                    if event::poll(timeout).expect("failed to poll new events")
-                        && READ_EVENT.load(Ordering::Relaxed)
-                    {
+                if READ_EVENT.load(Ordering::Relaxed) {
+                    if event::poll(timeout).expect("failed to poll new events") {
                         match event::read().expect("unable to read event") {
                             CrosstermEvent::Key(e) => sender.send(Event::Key(e)),
                             CrosstermEvent::Mouse(e) => sender.send(Event::Mouse(e)),
@@ -64,6 +59,8 @@ impl EventHandler {
                         }
                         .expect("failed to send terminal event")
                     }
+                } else {
+                    std::thread::sleep(timeout);
                 }
 
                 if last_tick.elapsed() >= tick_rate {
