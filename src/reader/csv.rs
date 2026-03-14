a6a486a92d97e08e894e7925ae889f4bbfcf57fc
shshemi <shshemi@gmail.com>
2026-03-14 15:05:19 +0000

    move csv reader to module

---
+++ src/reader/csv.rs
@@ -0,0 +1,96 @@
+use std::fs::File;
+
+use anyhow::anyhow;
+use polars::{
+    frame::DataFrame,
+    io::{SerReader, mmap::MmapBytesReader},
+    prelude::{CsvParseOptions, CsvReadOptions},
+};
+
+use crate::{
+    AppResult,
+    args::{Args, InferSchema},
+    misc::{stdin::stdin, type_ext::ToAscii},
+    reader::{NamedFrames, ReadToDataFrames, Source},
+};
+
+pub struct CsvToDataFrame {
+    infer_schema: InferSchema,
+    quote_char: char,
+    separator_char: char,
+    no_header: bool,
+    ignore_errors: bool,
+    truncate_ragged_lines: bool,
+}
+
+impl CsvToDataFrame {
+    pub fn from_args(args: &Args) -> Self {
+        Self {
+            infer_schema: args.infer_schema,
+            quote_char: args.quote_char,
+            separator_char: args.separator,
+            no_header: args.no_header,
+            ignore_errors: args.ignore_errors,
+            truncate_ragged_lines: args.truncate_ragged_lines,
+        }
+    }
+
+    pub fn with_separator(mut self, c: char) -> Self {
+        self.separator_char = c;
+        self
+    }
+
+    pub fn with_no_header(mut self, no_header: bool) -> Self {
+        self.no_header = no_header;
+        self
+    }
+
+    pub fn with_quote_char(mut self, c: char) -> Self {
+        self.quote_char = c;
+        self
+    }
+
+    fn try_into_frame(&self, reader: impl MmapBytesReader) -> AppResult<DataFrame> {
+        let df = CsvReadOptions::default()
+            .with_ignore_errors(self.ignore_errors)
+            .with_infer_schema_length(self.infer_schema.to_csv_infer_schema_length())
+            .with_has_header(!self.no_header)
+            .with_parse_options(
+                CsvParseOptions::default()
+                    .with_truncate_ragged_lines(self.truncate_ragged_lines)
+                    .with_quote_char(self.quote_char.to_ascii())
+                    .with_separator(
+                        self.separator_char
+                            .to_ascii()
+                            .ok_or(anyhow!("non-ASCII separator character"))?,
+                    ),
+            )
+            .with_rechunk(true)
+            .into_reader_with_file_handle(reader)
+            .finish()?;
+        Ok(df)
+    }
+}
+
+impl Default for CsvToDataFrame {
+    fn default() -> Self {
+        Self {
+            infer_schema: InferSchema::Safe,
+            quote_char: '"',
+            separator_char: ',',
+            no_header: false,
+            ignore_errors: true,
+            truncate_ragged_lines: false,
+        }
+    }
+}
+
+impl ReadToDataFrames for CsvToDataFrame {
+    fn named_frames(&self, input: Source) -> AppResult<NamedFrames> {
+        let df = match &input {
+            Source::File(path) => self.try_into_frame(File::open(path)?),
+            Source::Stdin => self.try_into_frame(stdin()),
+        }?;
+        Ok([(input.table_name(), df)].into())
+    }
+}
