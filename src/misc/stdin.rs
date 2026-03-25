b685b88ab5f830c69d8e0e07f10b84cbd5b720b9
shshemi <shshemi@gmail.com>
2026-03-25 22:02:21 +0000

    Use graceful shutdown for stdin read failure

--- src/misc/stdin.rs
+++ src/misc/stdin.rs
@@ -3,10 +3,14 @@ use std::{
     sync::LazyLock,
 };
 
+use super::type_ext::UnwrapOrGracefulShutdown;
+
 pub fn stdin() -> Cursor<&'static Vec<u8>> {
     static STDIN_CONTENT: LazyLock<Vec<u8>> = LazyLock::new(|| {
         let mut buf = Vec::new();
-        std::io::stdin().read_to_end(&mut buf).unwrap();
+        std::io::stdin()
+            .read_to_end(&mut buf)
+            .unwrap_or_graceful_shutdown();
         buf
     });
     Cursor::new(&STDIN_CONTENT)
