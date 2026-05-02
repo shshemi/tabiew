fbdd5d3ff14eb96267032aecb2536a5432ea8d17
shshemi <shshemi@gmail.com>
2026-05-03 00:57:40 +0100

    fix empty stdin

--- src/misc/stdin.rs
+++ src/misc/stdin.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{Cursor, Read},
+    io::{Cursor, IsTerminal, Read},
     sync::LazyLock,
 };
 
@@ -8,9 +8,11 @@ use super::type_ext::UnwrapOrGracefulShutdown;
 pub fn stdin() -> Cursor<&'static [u8]> {
     static STDIN_CONTENT: LazyLock<Vec<u8>> = LazyLock::new(|| {
         let mut buf = Vec::new();
-        std::io::stdin()
-            .read_to_end(&mut buf)
-            .unwrap_or_graceful_shutdown();
+        if std::io::stdin().is_terminal() {
+            std::io::stdin()
+                .read_to_end(&mut buf)
+                .unwrap_or_graceful_shutdown();
+        }
         buf
     });
     Cursor::new(&STDIN_CONTENT)
