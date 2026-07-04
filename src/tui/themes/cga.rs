09921f77aa4e5e91690450e9eb834f702de1301e
shshemi <shshemi@gmail.com>
2026-07-04 15:03:42 +0100

    fix cga theme

--- src/tui/themes/cga.rs
+++ src/tui/themes/cga.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for Cga {
     const BACKGROUND: Color = Color::from_u32(0x00000000);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00202020);
     const FOREGROUND: Color = Color::from_u32(0x00aaaaaa);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00000000);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x00606060);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00ff5555),
@@ -30,7 +30,7 @@ impl SixColorsTwoRowsStyler for Cga {
 
     const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00060606), Color::from_u32(0x000C0C0C)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x00989898);
-    const HIGHLIGHT_FOREGROUND: Color = Self::FOREGROUND;
+    const HIGHLIGHT_FOREGROUND: Color = Self::BACKGROUND;
 
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x007A0000);
 }
