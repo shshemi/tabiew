6c8e92898dc9275fde9c43fcec260c10b16648d0
shshemi <shshemi@gmail.com>
2026-07-07 00:27:15 +0100

    fix dark matrix theme

--- src/tui/themes/darkermatrix.rs
+++ src/tui/themes/darkermatrix.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for Darkermatrix {
     const BACKGROUND: Color = Color::from_u32(0x00070c0e);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00272C2E);
     const FOREGROUND: Color = Color::from_u32(0x004a6828);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00091013);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x003a5818);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x0000381d),
@@ -20,12 +20,12 @@ impl SixColorsTwoRowsStyler for Darkermatrix {
         Color::from_u32(0x00176c73),
     ];
     const DARK_COLORS: [Color; 6] = [
-        Color::from_u32(0x00002e18),
-        Color::from_u32(0x006fa64c),
-        Color::from_u32(0x00595900),
-        Color::from_u32(0x0000cb6b),
+        Color::from_u32(0x0000381d),
+        Color::from_u32(0x0090d762),
+        Color::from_u32(0x00e2e500),
+        Color::from_u32(0x0000ff87),
         Color::from_u32(0x00412a4d),
-        Color::from_u32(0x00125459),
+        Color::from_u32(0x00176c73),
     ];
 
     const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x000D1214), Color::from_u32(0x0013181A)];
