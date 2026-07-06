96a8cf593e119cfa55fe749b5d0343dd92460bb6
shshemi <shshemi@gmail.com>
2026-07-06 23:07:41 +0100

    fix ciapre theme

--- src/tui/themes/ciapre.rs
+++ src/tui/themes/ciapre.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for Ciapre {
     const BACKGROUND: Color = Color::from_u32(0x00191c27);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00393C47);
     const FOREGROUND: Color = Color::from_u32(0x00aea47a);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00181818);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x006c6238);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00ac3835),
@@ -20,10 +20,10 @@ impl SixColorsTwoRowsStyler for Ciapre {
         Color::from_u32(0x00f3dbb2),
     ];
     const DARK_COLORS: [Color; 6] = [
-        Color::from_u32(0x00810009),
-        Color::from_u32(0x0048513b),
-        Color::from_u32(0x00cc8b3f),
-        Color::from_u32(0x00576d8c),
+        Color::from_u32(0x00ac3835),
+        Color::from_u32(0x00a6a75d),
+        Color::from_u32(0x00dcdf7c),
+        Color::from_u32(0x003097c6),
         Color::from_u32(0x00724d7c),
         Color::from_u32(0x005c4f4b),
     ];
