fd33c5c632e3d48222e79efa0c85bd999d74180a
shshemi <shshemi@gmail.com>
2026-07-07 01:30:07 +0100

    fix django theme

--- src/tui/themes/django.rs
+++ src/tui/themes/django.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for Django {
     const BACKGROUND: Color = Color::from_u32(0x000b2f20);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x002B4F40);
     const FOREGROUND: Color = Color::from_u32(0x00f8f8f8);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00000000);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x00a8a8a8);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00ff943b),
@@ -30,7 +30,7 @@ impl SixColorsTwoRowsStyler for Django {
 
     const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00113526), Color::from_u32(0x00173B2C)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x002d6844);
-    const HIGHLIGHT_FOREGROUND: Color = Self::FOREGROUND;
+    const HIGHLIGHT_FOREGROUND: Color = Self::BACKGROUND;
 
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x00CFB832);
 }
