8e9499ab7d24e2642c1f21c123d0ee4ea80d1ac8
shshemi <shshemi@gmail.com>
2026-07-07 19:09:02 +0100

    fix tokyo night day theme

--- src/tui/themes/tokyo_night_day.rs
+++ src/tui/themes/tokyo_night_day.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for TokyoNightDay {
     const BACKGROUND: Color = Color::from_u32(0x00e1e2e7);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00FFFFFF);
     const FOREGROUND: Color = Color::from_u32(0x003760bf);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00e9e9ed);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x0087b0ff);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00f52a65),
@@ -28,9 +28,9 @@ impl SixColorsTwoRowsStyler for TokyoNightDay {
         Color::from_u32(0x00007197),
     ];
 
-    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00E7E8ED), Color::from_u32(0x00EDEEF3)];
+    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00E1E2E7), Color::from_u32(0x00EDEEF3)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x0017409F);
-    const HIGHLIGHT_FOREGROUND: Color = Self::FOREGROUND;
+    const HIGHLIGHT_FOREGROUND: Color = Self::BACKGROUND;
 
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x00C50035);
 }
