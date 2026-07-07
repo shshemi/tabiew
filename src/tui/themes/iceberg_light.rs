0a4ec5dee821bdb105cc526ff52f6579e45151e7
shshemi <shshemi@gmail.com>
2026-07-07 19:09:01 +0100

    fix iceberg light theme

--- src/tui/themes/iceberg_light.rs
+++ src/tui/themes/iceberg_light.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for IcebergLight {
     const BACKGROUND: Color = Color::from_u32(0x00e8e9ec);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00FFFFFF);
     const FOREGROUND: Color = Color::from_u32(0x0033374c);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00dcdfe7);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x0083879c);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00cc3768),
@@ -30,7 +30,7 @@ impl SixColorsTwoRowsStyler for IcebergLight {
 
     const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00EEEFF2), Color::from_u32(0x00F4F5F8)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x0013172C);
-    const HIGHLIGHT_FOREGROUND: Color = Self::FOREGROUND;
+    const HIGHLIGHT_FOREGROUND: Color = Self::BACKGROUND;
 
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x009C214A);
 }
