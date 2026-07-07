571625ac3e18eb8dc956b7e920eb4acc81e2e0a4
shshemi <shshemi@gmail.com>
2026-07-07 01:34:58 +0100

    fix dracula theme

--- src/tui/themes/dracula.rs
+++ src/tui/themes/dracula.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for Dracula {
     const BACKGROUND: Color = Color::from_u32(0x00282a36);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00484A56);
     const FOREGROUND: Color = Color::from_u32(0x00f8f8f2);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x0021222c);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x00a8a8a2);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00ff6e6e),
@@ -30,7 +30,7 @@ impl SixColorsTwoRowsStyler for Dracula {
 
     const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x002E303C), Color::from_u32(0x00343642)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x00D8D8D2);
-    const HIGHLIGHT_FOREGROUND: Color = Self::FOREGROUND;
+    const HIGHLIGHT_FOREGROUND: Color = Self::BACKGROUND;
 
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x00CF2525);
 }
