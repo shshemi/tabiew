f9023021773855c4c4d1bf52c228f3f83d1fab1c
shshemi <shshemi@gmail.com>
2026-07-07 19:09:02 +0100

    fix monokai pro light sun theme

--- src/tui/themes/monokai_pro_light_sun.rs
+++ src/tui/themes/monokai_pro_light_sun.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for MonokaiProLightSun {
     const BACKGROUND: Color = Color::from_u32(0x00f8efe7);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00FFFFFF);
     const FOREGROUND: Color = Color::from_u32(0x002c232e);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00f8efe7);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x007c737e);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00ce4770),
@@ -30,7 +30,7 @@ impl SixColorsTwoRowsStyler for MonokaiProLightSun {
 
     const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00FEF5ED), Color::from_u32(0x00FFFBF3)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x0052494D);
-    const HIGHLIGHT_FOREGROUND: Color = Self::FOREGROUND;
+    const HIGHLIGHT_FOREGROUND: Color = Self::BACKGROUND;
 
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x00A42700);
 }
