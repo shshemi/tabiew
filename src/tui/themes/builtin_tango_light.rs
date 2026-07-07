81ae0b5e10716ad5b6db29272998bbec3abfeedf
shshemi <shshemi@gmail.com>
2026-07-07 19:09:01 +0100

    fix builtin tango light theme

--- src/tui/themes/builtin_tango_light.rs
+++ src/tui/themes/builtin_tango_light.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for BuiltinTangoLight {
     const BACKGROUND: Color = Color::from_u32(0x00ffffff);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00FFFFFF);
     const FOREGROUND: Color = Color::from_u32(0x00000000);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00000000);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x00505050);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00ef2929),
