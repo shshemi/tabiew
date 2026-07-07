c137e3d3f902934ff89bdd64734fa71db82de24a
shshemi <shshemi@gmail.com>
2026-07-07 01:30:32 +0100

    fix doom one theme

--- src/tui/themes/doom_one.rs
+++ src/tui/themes/doom_one.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for DoomOne {
     const BACKGROUND: Color = Color::from_u32(0x00282c34);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00484C54);
     const FOREGROUND: Color = Color::from_u32(0x00bbc2cf);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00000000);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x006b727f);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00ff6655),
@@ -30,7 +30,7 @@ impl SixColorsTwoRowsStyler for DoomOne {
 
     const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x002E323A), Color::from_u32(0x00343840)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x00318FCF);
-    const HIGHLIGHT_FOREGROUND: Color = Self::FOREGROUND;
+    const HIGHLIGHT_FOREGROUND: Color = Self::BACKGROUND;
 
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x00CF3C3B);
 }
