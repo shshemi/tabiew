d921fe60f7d15454eaf8a67ff2b8d40d88a2d34e
shshemi <shshemi@gmail.com>
2026-07-07 19:09:02 +0100

    fix melange light theme

--- src/tui/themes/melange_light.rs
+++ src/tui/themes/melange_light.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for MelangeLight {
     const BACKGROUND: Color = Color::from_u32(0x00f1f1f1);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00FFFFFF);
     const FOREGROUND: Color = Color::from_u32(0x0054433a);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00e9e1db);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x00a4938a);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00bf0021),
@@ -30,7 +30,7 @@ impl SixColorsTwoRowsStyler for MelangeLight {
 
     const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00F7F7F7), Color::from_u32(0x00FDFDFD)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x0034231A);
-    const HIGHLIGHT_FOREGROUND: Color = Self::FOREGROUND;
+    const HIGHLIGHT_FOREGROUND: Color = Self::BACKGROUND;
 
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x00974B5B);
 }
