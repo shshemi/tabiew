e947b43723acb736384507c6cbbf72ef221e992e
shshemi <shshemi@gmail.com>
2026-07-07 19:09:02 +0100

    fix primary theme

--- src/tui/themes/primary.rs
+++ src/tui/themes/primary.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for Primary {
     const BACKGROUND: Color = Color::from_u32(0x00ffffff);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00FFFFFF);
     const FOREGROUND: Color = Color::from_u32(0x00000000);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00000000);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x00505050);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00db4437),
@@ -28,7 +28,7 @@ impl SixColorsTwoRowsStyler for Primary {
         Color::from_u32(0x004285f4),
     ];
 
-    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00FFFFFF), Color::from_u32(0x00FFFFFF)];
+    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00F0F0F0), Color::from_u32(0x00FFFFFF)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x004285f4);
     const HIGHLIGHT_FOREGROUND: Color = Color::from_u32(0x00ffffff);
 
