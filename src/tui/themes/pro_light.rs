e3a629fcbcfeae6375ac3b1baa324190997c87bf
shshemi <shshemi@gmail.com>
2026-07-07 19:09:02 +0100

    fix pro light theme

--- src/tui/themes/pro_light.rs
+++ src/tui/themes/pro_light.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for ProLight {
     const BACKGROUND: Color = Color::from_u32(0x00ffffff);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00FFFFFF);
     const FOREGROUND: Color = Color::from_u32(0x00191919);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00000000);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x00696969);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00ff6640),
@@ -28,9 +28,9 @@ impl SixColorsTwoRowsStyler for ProLight {
         Color::from_u32(0x004ed2de),
     ];
 
-    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00FFFFFF), Color::from_u32(0x00FFFFFF)];
+    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00F2F2F2), Color::from_u32(0x00FFFFFF)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x002D2D2D);
-    const HIGHLIGHT_FOREGROUND: Color = Self::FOREGROUND;
+    const HIGHLIGHT_FOREGROUND: Color = Self::BACKGROUND;
 
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x00BD36B8);
 }
