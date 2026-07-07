484fef97615c007ad6b8fe807e4cb696c6fc07e7
shshemi <shshemi@gmail.com>
2026-07-07 19:09:01 +0100

    fix ayu light theme

--- src/tui/themes/ayu_light.rs
+++ src/tui/themes/ayu_light.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for AyuLight {
     const BACKGROUND: Color = Color::from_u32(0x00fafafa);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00FFFFFF);
     const FOREGROUND: Color = Color::from_u32(0x005c6773);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00000000);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x00acb7c3);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00ff6565),
@@ -28,9 +28,9 @@ impl SixColorsTwoRowsStyler for AyuLight {
         Color::from_u32(0x004dbf99),
     ];
 
-    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00FFFFFF), Color::from_u32(0x00FFFFFF)];
+    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00F7F7F7), Color::from_u32(0x00FFFFFF)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x00DF4A00);
-    const HIGHLIGHT_FOREGROUND: Color = Self::FOREGROUND;
+    const HIGHLIGHT_FOREGROUND: Color = Self::BACKGROUND;
 
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x00CF0303);
 }
