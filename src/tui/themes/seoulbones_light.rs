27b77020a325bfbbf04cc49eb691512bab0d656a
shshemi <shshemi@gmail.com>
2026-07-07 19:09:02 +0100

    fix seoulbones light theme

--- src/tui/themes/seoulbones_light.rs
+++ src/tui/themes/seoulbones_light.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for SeoulbonesLight {
     const BACKGROUND: Color = Color::from_u32(0x00e2e2e2);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00FFFFFF);
     const FOREGROUND: Color = Color::from_u32(0x00555555);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00e2e2e2);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x00a5a5a5);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00be3c6d),
@@ -28,9 +28,9 @@ impl SixColorsTwoRowsStyler for SeoulbonesLight {
         Color::from_u32(0x00008586),
     ];
 
-    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00E8E8E8), Color::from_u32(0x00EEEEEE)];
+    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00E2E2E2), Color::from_u32(0x00EEEEEE)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x00353535);
-    const HIGHLIGHT_FOREGROUND: Color = Self::FOREGROUND;
+    const HIGHLIGHT_FOREGROUND: Color = Self::BACKGROUND;
 
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x00AC2254);
 }
