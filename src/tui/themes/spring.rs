f7b1b1e506f9b056213f72d0cd08acb00a60b455
shshemi <shshemi@gmail.com>
2026-07-07 19:09:02 +0100

    fix spring theme

--- src/tui/themes/spring.rs
+++ src/tui/themes/spring.rs
@@ -9,7 +9,7 @@ impl SixColorsTwoRowsStyler for Spring {
     const BACKGROUND: Color = Color::from_u32(0x00ffffff);
     const LIGHT_BACKGROUND: Color = Color::from_u32(0x00FFFFFF);
     const FOREGROUND: Color = Color::from_u32(0x004d4d4c);
-    const DARK_FOREGROUND: Color = Color::from_u32(0x00000000);
+    const DARK_FOREGROUND: Color = Color::from_u32(0x009d9d9c);
 
     const COLORS: [Color; 6] = [
         Color::from_u32(0x00ff0021),
@@ -28,9 +28,9 @@ impl SixColorsTwoRowsStyler for Spring {
         Color::from_u32(0x003e999f),
     ];
 
-    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00FFFFFF), Color::from_u32(0x00FFFFFF)];
+    const ROW_BACKGROUNDS: [Color; 2] = [Color::from_u32(0x00EDEDEC), Color::from_u32(0x00FFFFFF)];
     const HIGHLIGHT_BACKGROUND: Color = Color::from_u32(0x002D2D2C);
-    const HIGHLIGHT_FOREGROUND: Color = Self::FOREGROUND;
+    const HIGHLIGHT_FOREGROUND: Color = Self::BACKGROUND;
 
     const STATUS_BAR_ERROR: Color = Color::from_u32(0x00CF1D53);
 }
